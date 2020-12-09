075130d565cba1b13c069945cf29d4d2982494c5
khouilid <57531206+khouilid@users.noreply.github.com>
2020-12-09 18:42:15 +0100

    add article exercice

---
+++ src/Article/Article.java
@@ -0,0 +1,10 @@
+package Article;
+
+public class Article {
+    private String name ;
+    private String price;
+
+    public void afficher(){
+        System.out.println("le price de " + name + ":" + price);
+    }
+}
